fc13e3975385f20da5cc5626497cf42723bb72d0
KiPTM <133209150+KiPTM@users.noreply.github.com>
2024-06-23 09:51:18 +0000

    update

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-  return "Hello Applicants!"
-
+  return render_template('home.html')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True) 
