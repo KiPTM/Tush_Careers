443f9e7850ed4ce75c50af989cb2c2d8ae222906
KiPTM <133209150+KiPTM@users.noreply.github.com>
2024-06-25 13:09:54 +0000

    Nav bar and footer

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-JOBS = [
-  {
+JOBS = [{
     'id': 1,
     'title': 'Financial Analyst',
     'location': 'Nairobi, Kenya',
-    'salary': 'KES. 9000,000'},
-  {
+    'salary': 'KES. 9000,000'
+}, {
     'id': 2,
     'title': 'Data Scientist',
     'location': 'Cairo, Egypt',
-    'salary': 'USD. 10,000'},
-  {
+    'salary': 'USD. 10,000'
+}, {
     'id': 3,
     'title': 'Commercial Analyst',
     'location': 'New York, USA',
-    'salary': 'USD. 100,000'},
-  {
+    'salary': 'USD. 100,000'
+}, {
     'id': 4,
     'title': 'Backend Engineer',
-    'location': 'Cape Town, South Africa', 
+    'location': 'Cape Town, South Africa',
     'salary': 'Rand. 1,000,000'
-  }
-]
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html', 
-                         jobs=JOBS, 
-                         company_name='Tush')
-  
+  return render_template('home.html', jobs=JOBS, company_name='Kratos')
+
+
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
-  
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True) 
+  app.run(host='0.0.0.0', debug=True)
